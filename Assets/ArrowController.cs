07b431a83b0fe2dbedc54f3a4835fbd91ee32a28
kasamatsu <kasamatsu.shoji@gmail.com>
2022-02-18 07:33:54 +0900

    Hp実装

--- Assets/ArrowController.cs
+++ Assets/ArrowController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -19,6 +21,24 @@ public class ArrowController : MonoBehaviour
         //画面外に出たらオブジェクトを破壊する
         if(transform.position.y < -5.0f)
         {
+            Destroy(gameObject);
+        }
+
+        //当たり判定
+        Vector2 p1 = transform.position; //矢の中心座標
+        Vector2 p2 = this.player.transform.position; //プレイヤの中心座標
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude; //ベクトル「dir」の長さを求める
+        float r1 = 0.5f; //矢の半径
+        float r2 = 1.0f; //プレイヤの半径
+
+        if(d < r1 + r2)
+        {
+            //監督スクリプトにプレイヤと衝突したことを伝える
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+
+            //衝突した場合は矢を消す
             Destroy(gameObject);
         }
 
