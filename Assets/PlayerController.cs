10dcde321e7be5164ff13ae5b44735d19282cf7c
kasamatsu <kasamatsu.shoji@gmail.com>
2022-02-18 16:31:03 +0900

    CatEscape完成

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,11 +10,24 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    //左矢印が押されたとき
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0); //左に「3」動かす
+    }
+
+    //右矢印が押されたとき
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0); //右に「3」動かす
+    }
+
+    /*
     // Update is called once per frame
     void Update()
     {
         //左矢印が押されたとき
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.Translate(-3, 0, 0); //左に「3」動かす
         }
@@ -27,4 +40,5 @@ public class PlayerController : MonoBehaviour
 
         //上矢印「UpArrow」、下矢印「DownArrow」、Space「Space」
     }
+    */
 }
